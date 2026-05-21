ebb80e239f35e723334d05b4b8e24fbe25d8d1e8
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-21 23:44:44 +0300

    feat(api): implement central ApiServices registration logic

---
+++ FlowCore.Api/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FlowCore.Application;
+using FlowCore.Infrastructure;
+
+namespace FlowCore.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddControllers();
+
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+
+            service.AddInfrastructureServices(configuration);
+            service.AddApplicationServices(configuration);
+
+            return service;
+        }
+    }
+}
