1ac1b9528b055c1d4f63fcf203ac3d3ea7f92665
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-21 23:45:17 +0300

    feat(api): expose leave request endpoints via LeaveRequestsController

---
+++ FlowCore.Api/Controllers/LeaveRequestsController.cs
@@ -0,0 +1,34 @@
+﻿using FlowCore.Application.Features.LeaveRequests.Commands;
+using FlowCore.Application.Features.LeaveRequests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveRequestsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveRequestsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllLeaveRequestsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateLeaveRequestCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
