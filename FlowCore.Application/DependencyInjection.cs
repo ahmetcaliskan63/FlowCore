ada8eacdae4539a74fcb9d14978a6f43a176f474
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-21 23:39:33 +0300

    feat(application): register MediatR services in application layer

---
+++ FlowCore.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowCore.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+            return service;
+        }
+    }
+}
