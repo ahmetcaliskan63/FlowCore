9bb7224ef061fae288b989bbfaf041aad69afc3a
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-21 23:36:34 +0300

    feat(infrastructure): implement generic repository for db operations

---
+++ FlowCore.Infrastructure/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlowCore.Core.Common;
+using FlowCore.Core.Interfaces;
+using FlowCore.Infrastructure.Context; // AppDbContext burada olduğu için ekledik
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowCore.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => _context.Set<T>();
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
