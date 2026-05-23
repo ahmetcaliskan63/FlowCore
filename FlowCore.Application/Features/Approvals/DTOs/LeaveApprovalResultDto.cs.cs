2e36e4c464c156895b835029dd1ea12c4bac6e10
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-23 12:00:00 +0300

    feat(application): add LeaveApprovalResultDto to standardize approval response contracts

---
+++ FlowCore.Application/Features/Approvals/DTOs/LeaveApprovalResultDto.cs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.Approvals.DTOs
+{
+    public class LeaveApprovalResultDto
+    {
+        public Guid ApprovalId { get; set; }
+        public Guid LeaveRequestId { get; set; }
+        public string NewStatus { get; set; } = string.Empty;
+        public int RemainingLeaveCredits { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public DateTime ActionAt { get; set; }
+    }
+}
