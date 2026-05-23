1ef8aa52da4e4923ae172fef2cc06eeb6b8e1aa7
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-23 13:30:00 +0300

    feat(application): implement GetLeaveRequestsByUserId query to fetch user-specific requests

---
+++ FlowCore.Application/Features/Approvals/Queries/GetLeaveRequestsByUserIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using FlowCore.Application.Features.LeaveRequests.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.Approvals.Queries
+{
+    public class GetLeaveRequestsByUserIdQuery : IRequest<List<LeaveRequestDto>>
+    {
+
+    }
+}
