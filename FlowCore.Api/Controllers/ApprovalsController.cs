34ae8df692d0c4b7787e8a00de891f3cbb786a7a
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-24 14:30:00 +0300

    feat(api): add ApprovalsController to handle leave approval and rejection endpoints

---
+++ FlowCore.Api/Controllers/ApprovalsController.cs
@@ -0,0 +1,28 @@
+﻿using FlowCore.Application.Features.Approvals.Commands;
+using FlowCore.Application.Features.Approvals.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApprovalsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ApprovalsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("CreateLeaveApproval")]
+        public async Task<ActionResult<LeaveApprovalResultDto>> CreateLeaveApproval([FromBody] ApproveLeaveRequestCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
