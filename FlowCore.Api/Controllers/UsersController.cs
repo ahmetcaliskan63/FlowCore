f1bc120aa3eecca236e812b495823c86b4d5aec6
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-24 15:30:00 +0300

    feat(api): add UsersController to expose user management CRUD endpoints

---
+++ FlowCore.Api/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using FlowCore.Application.Features.Users.Commands;
+using FlowCore.Application.Features.Users.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllUserQuery();
+            var users = await _mediator.Send(query);
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            var query = new GetUserByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateUserCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest("ID uyuşmazlığı.");
+            }
+            command.Id = id;
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var command = new DeleteUserCommand { Id = id };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
