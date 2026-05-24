d6afb42f43000a30a892a37f4c64ada4ea79550d
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-24 10:00:00 +0300

    feat(application): add UserDto to encapsulate user data for API response contracts

---
+++ FlowCore.Application/Features/Users/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.Users.DTOs
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string DepartmentName { get; set; } = string.Empty;
+        public int TotalLeaveCredits { get; set; }
+        public int RemainingLeaveCredits { get; set; }
+
+    }
+}
