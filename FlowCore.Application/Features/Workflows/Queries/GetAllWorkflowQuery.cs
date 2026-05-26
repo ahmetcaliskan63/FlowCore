8efe08c628047758176147efb2d904da1ec3072e
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-27 01:23:10 +0300

    feat(application): add GetAllWorkflow and GetWorkflowById query handlers

---
+++ FlowCore.Application/Features/Workflows/Queries/GetAllWorkflowQuery.cs
@@ -0,0 +1,37 @@
+﻿using FlowCore.Application.Features.Workflows.DTOs;
+using FlowCore.Core.Entities;
+using FlowCore.Core.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.Workflows.Queries
+{
+    public class GetAllWorkflowQuery : IRequest<List<WorkflowDto>>
+    {
+    }
+    public class GetAllWorkflowQueryHandler : IRequestHandler<GetAllWorkflowQuery, List<WorkflowDto>>
+    {
+        private readonly IRepository<Workflow> _workflowRepository;
+        public GetAllWorkflowQueryHandler(IRepository<Workflow> workflowRepository)
+        {
+            _workflowRepository = workflowRepository;
+        }
+        public async Task<List<WorkflowDto>> Handle(GetAllWorkflowQuery request, CancellationToken cancellationToken)
+        {
+            var workflows = await _workflowRepository.Table
+                .Where(w => !w.IsDeleted)
+                .Select(w => new WorkflowDto
+                {
+                    Id = w.Id,
+                    WorkflowName = w.Name,
+                    Type = w.Type.ToString(),
+                    IsActive = w.IsActive
+                })
+                .ToListAsync(cancellationToken);
+            return workflows;
+        }
+    }
+}
---
+++ FlowCore.Application/Features/Workflows/Queries/GetWorkflowByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using FlowCore.Application.Features.Workflows.DTOs;
+using FlowCore.Core.Entities;
+using FlowCore.Core.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowCore.Application.Features.Workflows.Queries
+{
+    public class GetWorkflowByIdQuery : IRequest<WorkflowDto>
+    {
+        public Guid Id { get; set; }
+    }
+    public class GetWorkflowByIdQueryHandler : IRequestHandler<GetWorkflowByIdQuery, WorkflowDto>
+    {
+        private readonly IRepository<Workflow> _workflowRepository;
+        public GetWorkflowByIdQueryHandler(IRepository<Workflow> workflowRepository)
+        {
+            _workflowRepository = workflowRepository;
+        }
+        public async Task<WorkflowDto> Handle(GetWorkflowByIdQuery request, CancellationToken cancellationToken)
+        {
+            var workflow = await _workflowRepository.Table
+                .AsNoTracking()
+                .FirstOrDefaultAsync(w => w.Id == request.Id, cancellationToken);
+            if (workflow == null || workflow.IsDeleted)
+            {
+                throw new Exception("İş akışı bulunamadı.");
+            }
+            return new WorkflowDto
+            {
+                Id = workflow.Id,
+                WorkflowName = workflow.Name,
+                Type = workflow.Type.ToString(),
+                IsActive = workflow.IsActive
+            };
+        }
+    }
+}
