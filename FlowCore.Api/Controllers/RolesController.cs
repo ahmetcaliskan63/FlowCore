3f13488cdbe3e543392177eda9d0c715d6808806
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-27 01:28:30 +0300

    feat(api): add RolesController with CRUD endpoints

---
+++ FlowCore.Api/Controllers/RolesController.cs
@@ -0,0 +1,53 @@
+﻿using FlowCore.Application.Features.Roles.Commands;
+using FlowCore.Application.Features.Roles.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public RolesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet()]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var query = new GetAllRolesQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoleById(Guid id)
+        {
+            var query = new GetRoleByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole([FromBody] CreateRoleCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRole(Guid id, [FromBody] UpdateRoleCommand command)
+        {
+            command.Id = id;
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(Guid id)
+        {
+            var command = new DeleteRoleCommand { Id = id };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
