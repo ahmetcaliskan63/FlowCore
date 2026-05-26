aa153382812eef7c81b3859aab20a3e43cf86356
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-27 01:26:26 +0300

    feat(application): add GetAllDepartments and GetDepartmentById query handlers

---
+++ FlowCore.Application/Features/Departments/Queries/GetAllDepartmentsQuery.cs
@@ -0,0 +1,37 @@
+﻿using FlowCore.Application.Features.Departments.DTOs;
+using FlowCore.Core.Entities;
+using FlowCore.Core.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlowCore.Application.Features.Departments.Queries
+{
+    public class GetAllDepartmentsQuery : IRequest<List<DepartmentDto>>
+    {
+    }
+    public class GetAllDepartmentsQueryHandler : IRequestHandler<GetAllDepartmentsQuery, List<DepartmentDto>>
+    {
+        private readonly IRepository<Department> _departmentRepository;
+        public GetAllDepartmentsQueryHandler(IRepository<Department> departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        public async Task<List<DepartmentDto>> Handle(GetAllDepartmentsQuery request, CancellationToken cancellationToken)
+        {
+            var departments = await _departmentRepository.Table
+                .Where(u => !u.IsDeleted)
+                .Select(d => new DepartmentDto
+                {
+                    Id = d.Id,
+                    DepartmentName = d.DepartmentName,
+                })
+                .ToListAsync(cancellationToken);
+            return departments;
+        }
+    }
+
+}
---
+++ FlowCore.Application/Features/Departments/Queries/GetDepartmentByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using FlowCore.Application.Features.Departments.DTOs;
+using FlowCore.Core.Entities;
+using FlowCore.Core.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.Departments.Queries
+{
+    public class GetDepartmentByIdQuery : IRequest<DepartmentDto>
+    {
+        public Guid Id { get; set; }
+    }
+    public class GetDepartmentByIdQueryHandler : IRequestHandler<GetDepartmentByIdQuery, DepartmentDto>
+    {
+        private readonly IRepository<Department> _departmentRepository;
+        public GetDepartmentByIdQueryHandler(IRepository<Department> departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        public async Task<DepartmentDto> Handle(GetDepartmentByIdQuery request, CancellationToken cancellationToken)
+        {
+            var department = await _departmentRepository.Table
+                .FirstOrDefaultAsync(d => d.Id == request.Id && !d.IsDeleted, cancellationToken);
+            if (department == null)
+            {
+                throw new Exception($"'{request.Id}'bu id'ye sahip bir departman bulunamadı.");
+            }
+            return new DepartmentDto
+            {
+                Id = department.Id,
+                DepartmentName = department.DepartmentName
+            };
+        }
+    }
+}
