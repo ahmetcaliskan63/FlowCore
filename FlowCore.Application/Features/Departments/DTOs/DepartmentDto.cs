ba2218c9696d524d43692fb6e9099a4ad2857b80
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-27 01:26:49 +0300

    feat(application): add DepartmentDto with Id and DepartmentName properties

---
+++ FlowCore.Application/Features/Departments/DTOs/DepartmentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.Departments.DTOs
+{
+    public class DepartmentDto
+    {
+        public Guid Id { get; set; }
+        public string DepartmentName { get; set; } = string.Empty;
+
+    }
+}
