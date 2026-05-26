7f389ea2b92ce5232633d1b4ec580e3c231a3e87
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-27 01:21:36 +0300

    feat(application): add WorkflowSteps feature scaffold with DTO, Commands, and Queries structure

---
+++ FlowCore.Application/Features/WorkflowSteps/DTOs/WorkflowStepsDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlowCore.Application.Features.WorkflowSteps.DTOs
+{
+    internal class WorkflowStepsDTO
+    {
+    }
+}
