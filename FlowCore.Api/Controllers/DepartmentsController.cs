20b41fef0bcf8901c1b33150be80c4c26e29818d
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-27 01:28:52 +0300

    feat(api): add DepartmentsController with CRUD endpoints

---
+++ FlowCore.Api/Controllers/DepartmentsController.cs
@@ -0,0 +1,54 @@
+﻿using FlowCore.Application.Features.Departments.Commands;
+using FlowCore.Application.Features.Departments.DTOs;
+using FlowCore.Application.Features.Departments.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public DepartmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            var query = new GetAllDepartmentsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDepartmentById(Guid id)
+        {
+            var query = new GetDepartmentByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateDepartment([FromBody] CreateDepartmentCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDepartment(Guid id, [FromBody] UpdateDepartmentCommand command)
+        {
+            command.Id = id;
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(Guid id)
+        {
+            var command = new DeleteDepartmentCommand { Id = id };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
