0c821a4ecdc9dacc55412b03aacd40850ce97bec
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-05-29 03:29:00 +0300

    Add Constants

---
+++ FlowCore.Core/Constants/SystemRoles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FlowCore.Core.Constants
+{
+    public static class SystemRoles
+    {
+        public const string Manager = "Manager";
+        public const string HR = "HR";
+        public const string CEO = "CEO";
+
+        public static readonly List<string> CanApproveLeaveRequests = new()
+        {
+            Manager,
+            HR,
+            CEO
+        };
+    }
+}
