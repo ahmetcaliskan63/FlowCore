8c57386e75bc859ce8228205ac554b17a86bc716
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-06 15:07:34 +0300

    Update MappingProfile with UserDto and ApprovalDto mappings

--- FlowCore.Application/Common/Mappings/MappingProfile.cs
+++ FlowCore.Application/Common/Mappings/MappingProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using FlowCore.Application.Features.Approvals.DTOs;
 using FlowCore.Application.Features.Departments.DTOs;
 using FlowCore.Application.Features.Roles.DTOs;
 using FlowCore.Application.Features.Users.DTOs;
@@ -12,7 +13,14 @@ namespace FlowCore.Application.Common.Mappings
         {
             CreateMap<Role, RoleDto>();
             CreateMap<Department, DepartmentDto>();
-            CreateMap<User, UserDto>();
+            CreateMap<User, UserDto>().ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department != null ? src.Department.DepartmentName :"Departman Atanmamış"));
+            CreateMap<Approval, ApprovalDto>()
+                .ForMember(dest => dest.ApproverFullName,
+                           opt => opt.MapFrom(src => src.ApproverByUser != null ? src.ApproverByUser.FullName : "Sistem"))
+                .ForMember(dest => dest.ApproverRole,
+                           opt => opt.MapFrom(src => src.ApproverByUser != null && src.ApproverByUser.Role != null ? src.ApproverByUser.Role.Name : "Rol Tanımsız"))
+                .ForMember(dest => dest.Status,
+                           opt => opt.MapFrom(src => src.Status.ToString()));
         }
     }
 }
