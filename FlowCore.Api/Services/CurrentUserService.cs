0a7d4293e625cfcaf9d09f4a6f882809df1b14b8
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-11 15:00:00 +0300

    Add CurrentUserService

---
+++ FlowCore.Api/Services/CurrentUserService.cs
@@ -0,0 +1,21 @@
+using FlowCore.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace FlowCore.Api.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId =>
+            _httpContextAccessor.HttpContext?.User?.FindFirstValue(JwtRegisteredClaimNames.Sub)
+            ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
