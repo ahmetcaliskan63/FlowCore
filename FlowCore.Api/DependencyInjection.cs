69c2fdf545101c2189a19f53c440b8f877c024b2
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-11 10:00:00 +0300

    Update API DependencyInjection

--- FlowCore.Api/DependencyInjection.cs
+++ FlowCore.Api/DependencyInjection.cs
@@ -1,5 +1,10 @@
 using FlowCore.Application;
+using FlowCore.Core.Interfaces;
 using FlowCore.Infrastructure;
+using FlowCore.Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace FlowCore.Api
 {
@@ -11,6 +16,31 @@ namespace FlowCore.Api
 
             service.AddEndpointsApiExplorer();
             service.AddSwaggerGen();
+
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings["Secret"];
+
+            service.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
+                };
+            });
+
+            service.AddHttpContextAccessor();
+            service.AddScoped<ICurrentUserService, CurrentUserService>();
 
             service.AddInfrastructureServices(configuration);
             service.AddApplicationServices(configuration);
