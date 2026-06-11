f783da3621f7d2da9011bb8579a8e9c372ccb0c9
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-11 12:00:00 +0300

    Update Program.cs configuration

--- FlowCore.Api/Program.cs
+++ FlowCore.Api/Program.cs
@@ -5,23 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApiServices(builder.Configuration);
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await FlowCore.Infrastructure.Context.DataSeeder.SeedAsync(scope.ServiceProvider);
+}
+
+app.UseMiddleware<FlowCore.Api.Middleware.GlobalExceptionHandlerMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
