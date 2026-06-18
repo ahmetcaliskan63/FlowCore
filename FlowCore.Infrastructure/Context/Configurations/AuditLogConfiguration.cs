631dc0a16a0767fed4c26eb2ced9ea0f2dfd47d1
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-18 12:00:00 +0300

    feat(data): add AuditLog entity configuration

---
+++ FlowCore.Infrastructure/Context/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,27 @@
+using FlowCore.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlowCore.Infrastructure.Context.Configurations
+{
+    public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
+    {
+        public void Configure(EntityTypeBuilder<AuditLog> builder)
+        {
+            builder.ToTable("AuditLogs");
+
+            builder.Property(a => a.Action)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(a => a.EntityName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasOne(a => a.User)
+                .WithMany()
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
