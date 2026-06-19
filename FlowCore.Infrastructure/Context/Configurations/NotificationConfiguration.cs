674a611c27f349115c318f4bbd6a9e5bd578bb9c
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-19 14:50:29 +0300

    feat(data): add Notification entity configuration

---
+++ FlowCore.Infrastructure/Context/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,27 @@
+using FlowCore.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlowCore.Infrastructure.Context.Configurations
+{
+    public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.ToTable("Notifications");
+
+            builder.Property(n => n.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(n => n.Message)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.HasOne(n => n.User)
+                .WithMany()
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
