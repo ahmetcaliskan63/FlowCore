8d731974b964a205080d717020ae32c2c4fcbb8c
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-20 18:00:00 +0300

    Refactor generic Repository implementation

--- FlowCore.Infrastructure/Repository.cs
+++ FlowCore.Infrastructure/Repository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -33,19 +33,18 @@ namespace FlowCore.Infrastructure
         public async Task AddAsync(T entity)
         {
             await _context.Set<T>().AddAsync(entity);
-            await _context.SaveChangesAsync();
         }
 
-        public async Task UpdateAsync(T entity)
+        public Task UpdateAsync(T entity)
         {
             _context.Set<T>().Update(entity);
-            await _context.SaveChangesAsync();
+            return Task.CompletedTask;
         }
 
-        public async Task DeleteAsync(T entity)
+        public Task DeleteAsync(T entity)
         {
             _context.Set<T>().Remove(entity);
-            await _context.SaveChangesAsync();
+            return Task.CompletedTask;
         }
     }
 }
