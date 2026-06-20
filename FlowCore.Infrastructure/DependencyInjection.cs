3fa5da2e5e475bb053196d027f1ba5ed58d752dd
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-20 16:00:00 +0300

    Update DependencyInjection for new services

--- FlowCore.Infrastructure/DependencyInjection.cs
+++ FlowCore.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,11 @@
-﻿using FlowCore.Core.Interfaces;
+using FlowCore.Core.Interfaces;
+using FlowCore.Infrastructure.Authentication;
 using FlowCore.Infrastructure.Context;
+using FlowCore.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace FlowCore.Infrastructure
 {
@@ -13,6 +16,13 @@ namespace FlowCore.Infrastructure
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IEmailService, EmailService>();
+
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+
             return services;
         }
     }
