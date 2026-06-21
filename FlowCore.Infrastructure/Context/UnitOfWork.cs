d9bd6238a2bdf711d829d1424afed110814ef783
ahmetcaliskan63 <ahmetabclife@gmail.com>
2026-06-21 16:00:00 +0300

    Implement UnitOfWork

---
+++ FlowCore.Infrastructure/Context/UnitOfWork.cs
@@ -0,0 +1,19 @@
+using FlowCore.Core.Interfaces;
+
+namespace FlowCore.Infrastructure.Context
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
